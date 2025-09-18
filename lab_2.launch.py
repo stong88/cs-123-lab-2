702b9e86a52a1395765ee89df275466fab4e7fbc
Nathan Kau <nathankau@gmail.com>
2025-09-18 14:25:34 -0700

    Fix paths. Use left leg. Add foxglove layout json file

--- lab_2.launch.py
+++ lab_2.launch.py
@@ -1,18 +1,3 @@
-# Copyright 2021 Stogl Robotics Consulting UG (haftungsbeschränkt)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 from launch import LaunchDescription
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
@@ -20,6 +5,8 @@ from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
+import os
 
 
 def generate_launch_description():
@@ -30,29 +17,23 @@ def generate_launch_description():
             " ",
             PathJoinSubstitution(
                 [
-#                    FindPackageShare("control_board_hardware_interface"),
-                    # "/home/pi/ros2_ws/src/control_board_hardware_interface",
-                    # "test",
-                    # "test_state_publisher.urdf.xacro",
-                    "/home/pi/ros2_ws/src/pupper_v3_description",
+                    FindPackageShare("pupper_v3_description"),
                     "description",
                     "pupper_v3.urdf.xacro",
                 ]
             ),
         ]
     )
+
     robot_description = {"robot_description": robot_description_content}
 
+    # Get config file relative to this launch file
     robot_controllers = PathJoinSubstitution(
         [
-#            FindPackageShare("control_board_hardware_interface"),
-            "/home/pi/lab_2",
+            os.path.dirname(__file__),
             "lab_2.yaml",
         ]
     )
-    # rviz_config_file = PathJoinSubstitution(
-    #     [FindPackageShare("ros2_control_demo_example_1"), "rviz", "rrbot.rviz"]
-    # )
 
     control_node = Node(
         package="controller_manager",
@@ -66,68 +47,69 @@ def generate_launch_description():
         output="both",
         parameters=[robot_description],
     )
-    # rviz_node = Node(
-    #     package="rviz2",
-    #     executable="rviz2",
-    #     name="rviz2",
-    #     output="log",
-    #     arguments=["-d", rviz_config_file],
-    # )
-#    joy_node = Node(
-#        package="joy_linux",
-#        executable="joy_linux_node",
-#        output="both",
-#    )
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager", "--controller-manager-timeout", "30"],
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+            "--controller-manager-timeout",
+            "30",
+        ],
     )
 
     imu_sensor_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["imu_sensor_broadcaster", "--controller-manager", "/controller_manager", "--controller-manager-timeout", "30"],
+        arguments=[
+            "imu_sensor_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+            "--controller-manager-timeout",
+            "30",
+        ],
     )
 
-    robot_controller_spawner = Node(
+    kp_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["forward_command_controller", "--controller-manager", "/controller_manager", "--controller-manager-timeout", "30"],
+        arguments=[
+            "forward_kp_controller",
+            "--controller-manager",
+            "/controller_manager",
+            "--controller-manager-timeout",
+            "30",
+        ],
     )
 
-    # Delay rviz start after `joint_state_broadcaster`
-    # delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=joint_state_broadcaster_spawner,
-    #         on_exit=[rviz_node],
-    #     )
-    # )
+    kd_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "forward_kd_controller",
+            "--controller-manager",
+            "/controller_manager",
+            "--controller-manager-timeout",
+            "30",
+        ],
+    )
 
-    # delay_joint_state_broadcaster_spawner_after_control_node = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=joint_state_broadcaster_spawner,
-    #         on_exit=[control_node],
-    #     )
-    # )
-
-    # Delay start of robot_controller after `joint_state_broadcaster`
-    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[robot_controller_spawner],
-        )
+    foxglove_bridge = Node(
+        package="foxglove_bridge",
+        executable="foxglove_bridge",
+        output="both",
     )
 
     nodes = [
         control_node,
- #       joy_node,
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
         imu_sensor_broadcaster_spawner,
-        # delay_rviz_after_joint_state_broadcaster_spawner,
-        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        kp_spawner,
+        kd_spawner,
+        foxglove_bridge,
     ]
 
     return LaunchDescription(nodes)
--- lab_2.py
+++ lab_2.py
@@ -5,90 +5,91 @@ from std_msgs.msg import Float64MultiArray
 from visualization_msgs.msg import Marker
 import numpy as np
 
+
 class ForwardKinematics(Node):
 
     def __init__(self):
-        super().__init__('forward_kinematics')
-        self.joint_subscription = self.create_subscription(
-            JointState,
-            'joint_states',
-            self.listener_callback,
-            10)
+        super().__init__("forward_kinematics")
+        self.joint_subscription = self.create_subscription(JointState, "joint_states", self.listener_callback, 10)
         self.joint_subscription  # prevent unused variable warning
 
-        self.position_publisher = self.create_publisher(
-            Float64MultiArray,
-            'leg_front_r_end_effector_position',
-            10)
-
-        self.marker_publisher = self.create_publisher(
-            Marker,
-            'marker',
-            10)
-
-        timer_period = 0.1  # publish FK information and marker at 10Hz
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.position_publisher = self.create_publisher(Float64MultiArray, "leg_front_l_end_effector_position", 10)
+        self.marker_publisher = self.create_publisher(Marker, "marker", 10)
 
         self.joint_positions = None
+        timer_period = 0.02  # publish FK information and marker at 50Hz
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        self.kp_publisher = self.create_publisher(Float64MultiArray, "/forward_kp_controller/commands", 10)
+        self.kd_publisher = self.create_publisher(Float64MultiArray, "/forward_kd_controller/commands", 10)
+
+        # Periodically set gains to 0 so legs go limp
+        self.create_timer(0.1, self.publish_zero_gains)
+
+    def publish_zero_gains(self):
+        self.kp_publisher.publish(Float64MultiArray(data=[0.0] * 12))
+        self.kd_publisher.publish(Float64MultiArray(data=[0.0] * 12))
 
     def listener_callback(self, msg):
-        # Extract the positions of the joints related to leg_front_r
-        joints_of_interest = ['leg_front_r_1', 'leg_front_r_2', 'leg_front_r_3']
+        # Extract the positions of the joints related to leg_front_l
+        joints_of_interest = ["leg_front_l_1", "leg_front_l_2", "leg_front_l_3"]
         self.joint_positions = [msg.position[msg.name.index(joint)] for joint in joints_of_interest]
 
     def forward_kinematics(self, theta1, theta2, theta3):
 
         def rotation_x(angle):
             # rotation about the x-axis implemented for you
-            return np.array([
-                [1, 0, 0, 0],
-                [0, np.cos(angle), -np.sin(angle), 0],
-                [0, np.sin(angle), np.cos(angle), 0],
-                [0, 0, 0, 1]
-            ])
+            return np.array(
+                [
+                    [1, 0, 0, 0],
+                    [0, np.cos(angle), -np.sin(angle), 0],
+                    [0, np.sin(angle), np.cos(angle), 0],
+                    [0, 0, 0, 1],
+                ]
+            )
 
         def rotation_y(angle):
-            return 
             ## TODO: Implement the rotation matrix about the y-axis
             # return np.array([
             # ])
-        
+            raise NotImplementedError()
+
         def rotation_z(angle):
-            return
             ## TODO: Implement the rotation matrix about the z-axis
             # return np.array([
             # ])
+            raise NotImplementedError()
 
         def translation(x, y, z):
             ## TODO: Implement the translation matrix
             # return np.array([
             # ])
-            None
+            raise NotImplementedError()
 
-        # T_0_1 (base_link to leg_front_r_1)
-        T_0_1 = translation(0.07500, -0.0445, 0) @ rotation_x(1.57080) @ rotation_z(theta1)
+        # T_0_1 (base_link to leg_front_l_1)
+        T_0_1 = translation(0.07500, 0.0445, 0) @ rotation_x(-1.57080) @ rotation_z(theta1)
 
-        # T_1_2 (leg_front_r_1 to leg_front_r_2)
-        ## TODO: Implement the transformation matrix from leg_front_r_1 to leg_front_r_2
+        # T_1_2 (leg_front_l_1 to leg_front_l_2)
+        ## TODO: Implement the transformation matrix from leg_front_l_1 to leg_front_l_2
         T_1_2 = None
 
-        # T_2_3 (leg_front_r_2 to leg_front_r_3)
-        ## TODO: Implement the transformation matrix from leg_front_r_2 to leg_front_r_3
+        # T_2_3 (leg_front_l_2 to leg_front_l_3)
+        ## TODO: Implement the transformation matrix from leg_front_l_2 to leg_front_l_3
         T_2_3 = None
 
-        # T_3_ee (leg_front_r_3 to end-effector)
+        # T_3_ee (leg_front_l_3 to end-effector)
         T_3_ee = None
 
-        # TODO: Compute the final transformation. T_0_ee is a concatenation of the previous transformation matrices
+        # TODO: Compute the final transformation. T_0_ee is the multiplication of the previous transformation matrices
         T_0_ee = None
 
-        # TODO: Extract the end-effector position. The end effector position is a 3x3 matrix (not in homogenous coordinates)
+        # TODO: Extract the end-effector position. The end effector position is a 3x1 vector (not in homogenous coordinates)
         end_effector_position = None
 
         return end_effector_position
 
-
     def timer_callback(self):
+        """Timer callback for publishing end-effector marker and position."""
         if self.joint_positions is not None:
             # Joint angles
             theta1 = self.joint_positions[0]
@@ -98,7 +99,7 @@ class ForwardKinematics(Node):
             end_effector_position = self.forward_kinematics(theta1, theta2, theta3)
 
             marker = Marker()
-            marker.header.frame_id = '/base_link'
+            marker.header.frame_id = "/base_link"
             marker.header.stamp = self.get_clock().now().to_msg()
             marker.type = marker.SPHERE
             marker.id = 0
@@ -117,8 +118,10 @@ class ForwardKinematics(Node):
             position = Float64MultiArray()
             position.data = end_effector_position
             self.position_publisher.publish(position)
-            # self.get_logger().info(f'theta1 = {theta1:.1f}, theta2 = {theta2:.1f}, theta3 = {theta3:.1f}')
-            self.get_logger().info(f'End-Effector Position: x={end_effector_position[0]:.2f}, y={end_effector_position[1]:.2f}, z={end_effector_position[2]:.2f}')
+            self.get_logger().info(
+                f"End-Effector Position: x={end_effector_position[0]:.2f}, y={end_effector_position[1]:.2f}, z={end_effector_position[2]:.2f}"
+            )
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -127,8 +130,6 @@ def main(args=None):
 
     rclpy.spin(forward_kinematics)
 
-    forward_kinematics.destroy_node()
-    rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

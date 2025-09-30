f38592866476c2663f7a347885f30d086d24153b
stong88 <70980554+stong88@users.noreply.github.com>
2025-09-29 18:02:13 -0700

    adjust translation values

--- lab_2.py
+++ lab_2.py
@@ -4,7 +4,7 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray
 from visualization_msgs.msg import Marker
 import numpy as np
-
+import math
 
 class ForwardKinematics(Node):
 
@@ -79,18 +79,18 @@ class ForwardKinematics(Node):
             )
 
         # T_0_1 (base_link to leg_front_l_1)
-        T_0_1 = translation(0.07500, 0.0445, 0) @ rotation_x(-1.57080) @ rotation_z(theta1)
+        T_0_1 = translation(0.07500, 0.0445, 0) @ rotation_x(1.57080) @ rotation_z(-theta1)
 
         # T_1_2 (leg_front_l_1 to leg_front_l_2)
         ## TODO: Implement the transformation matrix from leg_front_l_1 to leg_front_l_2
-        T_1_2 = translation(0.0, 0.0, -0.0825) @ rotation_y(theta2)
+        T_1_2 = translation(0.0, 0.0, -0.039) @ rotation_y(-math.pi / 2) @ rotation_z(theta2)
 
         # T_2_3 (leg_front_l_2 to leg_front_l_3)
         ## TODO: Implement the transformation matrix from leg_front_l_2 to leg_front_l_3
-        T_2_3 = translation(0.0, 0.0, -0.2) @ rotation_y(theta3)
+        T_2_3 = translation(0.0, -0.0494, 0.0685) @ rotation_y(math.pi/2) @ rotation_z(-theta3)
 
         # T_3_ee (leg_front_l_3 to end-effector)
-        T_3_ee = translation(0.0, 0.0, -0.2) @ translation(0.0, 0.0, -0.08)
+        T_3_ee = translation(0.06231, -0.06216, -0.018)
 
         # TODO: Compute the final transformation. T_0_ee is the multiplication of the previous transformation matrices
         T_0_ee = T_0_1 @ T_1_2 @ T_2_3 @ T_3_ee
